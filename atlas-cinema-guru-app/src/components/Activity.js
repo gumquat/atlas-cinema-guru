79306381911bd9108c4f16e6546ade0b813b0552
gumquat <gumquat@gmail.com>
2024-07-08 15:54:08 -0500

    tasks: style sidebar, finish activity.js

--- atlas-cinema-guru-app/src/components/Activity.js
+++ atlas-cinema-guru-app/src/components/Activity.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './components.css'
 
 const Activity = () => {
-  return
+  return (
+    <div>        
+      <h2>Activities</h2>
+    </div>
+  )
 }
 
 export default Activity;

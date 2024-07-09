4706c1e90500badacf9108d87de09a89eb5501a1
gumquat <gumquat@gmail.com>
2024-07-09 14:27:31 -0500

    prepping dashboard for task 9 and 10. 8 completed

--- atlas-cinema-guru-app/src/routes/dashboard/Dashboard.js
+++ atlas-cinema-guru-app/src/routes/dashboard/Dashboard.js
@@ -4,6 +4,11 @@ import Header from '../../components/navigation/Header'
 import SideBar from '../../components/navigation/SideBar'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+//placehodlers
+const HomePage = () => <div></div>;
+const Favorites = () => <div></div>;
+const WatchLater = () => <div></div>;
+
 const Dashboard = ({userUsername, setIsLoggedIn}) => {
   return (
     <BrowserRouter>
@@ -11,9 +16,9 @@ const Dashboard = ({userUsername, setIsLoggedIn}) => {
         <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
         <SideBar />
         <Routes>
-          <Route path="/home"/>
-          <Route path="/favorites"/>
-          <Route path="/watchlater"/>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/watchlater" element={<WatchLater />} />
           <Route path="*" element={<Navigate replace to="/home" />} />
         </Routes>
       </div>
